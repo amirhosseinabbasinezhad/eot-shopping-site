9050342579cf333a2512cb4d02398d68618d225c
AmirHosseinAbbasiNezhad <amirabbasinezhad6@gmail.com>
2022-03-24 19:18:08 +0430

    c2

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -1,27 +1,50 @@
 import { faUser ,faCartShopping,faBars } from "@fortawesome/free-solid-svg-icons";
+import mylogo from './mylogo.svg'
+import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import './styleHeading.css';
 function Heading(props) {
     return (<>
-        <div className="row">   
-            <div className="col-3 user-cart">
-                <FontAwesomeIcon className="ficon" icon={faUser}size="2x" />
-                <FontAwesomeIcon className="ficon" icon={faCartShopping}size="2x" />
-                <div class="searchbar">
-                <form>
-                    <input type="text" name="search" value="" placeholder="مثلا : قاب آیفون"/>
-                    <button type="button" name="button"><i class="fas fa-search"></i></button>
-                </form>
+    <div className="header col-12" >
+        <div className="row fr col-12" style={{margin:'0px',padding:'0px', bsGutterX:" 0"}}>
+            <div className="col-11">
+                <div className="top row">
+                    <div className=" left-icons col-2">
+                        <FontAwesomeIcon className="ficon" icon={faUser}size="2x" />
+                        <FontAwesomeIcon className="ficon" icon={faCartShopping}size="2x" />
+                    </div>
+                    <div className="searchbar col-8">
+                        <form action="#">
+                            <input type="text" />
+                            <Button variant="primary">Primary</Button>
+                        </form>
+                    </div>
+                </div>
+                <div className="line-bottom-right"></div>
+            </div>
+            
+            <div className="mainlogo col-1">
+                <img src={mylogo} alt="React Logo" />
+            </div>
+            <div className="row menu">
+                <div className="col-11">
+                    <ul>
+                        <li>صفحه اصلی</li>
+                        <li>صفحه اصلی</li>
+                        <li>صفحه اصلی</li>
+                        <li>صفحه اصلی</li>
+                    </ul>
+                </div>
+                <div className="col-1">
+                    <div className="openbtn" onClick={props.onClick}>
+                        <FontAwesomeIcon icon={faBars} size="xl" />
+                    </div>
                 </div>
             </div>
-            <div className="col-3 storename">my store</div>
-            <div className="col-3">
-                <button className="openbtn" onClick={props.onClick}>
-                <FontAwesomeIcon icon={faBars} />
-                </button>
-            </div>
         </div>
+    </div>
+        
     </>  );
 }
 
